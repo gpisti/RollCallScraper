3b89ebc9454cc62359184c84f684f1829bc1587d
gpisti <113467025+gpisti@users.noreply.github.com>
2024-10-19 16:30:56 +0200

    Update web_driver.py

--- web_driver.py
+++ web_driver.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 def initialize_web_driver():
     """Initializing a WebDriver using Brave browser"""
     brave_path = "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe" # <-- MÁS GÉP ESETÉN ÁTÍRANDÓ ADOTT ELÉRÉSI ÚTVONALRA
+    #Támogat minden Chromium alapú böngészőt! (A változó azért van így elnevezve, mert én Brave Browser-t használtam)
     chrome_options = Options()
     chrome_options.binary_location = brave_path
 
