0f534912fadbcc55bcc11a67c37ca923e1f01112
gpisti <113467025+gpisti@users.noreply.github.com>
2024-10-25 16:22:59 +0200

    Update web_driver.py

--- web_driver.py
+++ web_driver.py
@@ -2,14 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
+
 def initialize_web_driver():
     """Initializing a WebDriver using Brave browser"""
-    brave_path = "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe" # <-- MÁS GÉP ESETÉN ÁTÍRANDÓ ADOTT ELÉRÉSI ÚTVONALRA
-    #Támogat minden Chromium alapú böngészőt! (A változó azért van így elnevezve, mert én Brave Browser-t használtam)
+    brave_path = "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"  # <-- MÁS GÉP ESETÉN ÁTÍRANDÓ ADOTT ELÉRÉSI ÚTVONALRA
+    # Támogat minden Chromium alapú böngészőt! (A változó azért van így elnevezve, mert én Brave Browser-t használtam)
     chrome_options = Options()
     chrome_options.binary_location = brave_path
 
-    chrome_driver_path = "D:\\Prog\\Chromium\\chromedriver-win64\\chromedriver.exe" # <-- MÁS GÉP ESETÉN ÁTÍRANDÓ ADOTT ELÉRÉSI ÚTVONALÁRA
+    chrome_driver_path = "D:\\Prog\\Chromium\\chromedriver-win64\\chromedriver.exe"  # <-- MÁS GÉP ESETÉN ÁTÍRANDÓ ADOTT ELÉRÉSI ÚTVONALÁRA
     service = Service(chrome_driver_path)
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
