54220b665ecfec6fdf63ea8573b3e0576041906d
gpisti <113467025+gpisti@users.noreply.github.com>
2024-10-25 16:24:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,27 +4,42 @@ from data_handler import save_data_to_csv
 import os
 import pandas as pd
 
+
 def main():
     driver = initialize_web_driver()
 
     try:
-        #SZÜKSÉGES ADATOK SCRAPELÉSE
-        roll_call_votes, tally = scrape_votes(driver)
+        # SZÜKSÉGES ADATOK SCRAPELÉSE
+        data = scrape_votes(driver)
 
-        # LISTÁK ÁTALAKÁTÁSA -> DATAFRAME
-        df_roll_call_votes = pd.DataFrame(roll_call_votes)
-        df_tally = pd.DataFrame(tally)
+        flat_data = []
+        for roll_call in data:
+            for vote in roll_call["votes"]:
+                
+                # ÖSSZEÁGYAZOTT ADATHALMAT KISIMÍTÁSA A CSV-HEZ
+                flat_data.append(
+                    {
+                        "Date of the vote": roll_call["date"],
+                        "Vote result": roll_call["vote result"],
+                        "Measure/Nominant Number": roll_call["m/n.number"],
+                        "Measure Title/Nomination Description": roll_call["title/description"],
+                        "Senator's name": vote["Senator's name"],
+                        "Senator's voting record": vote["Vote"],
+                        "Senator's party affiliation": vote["Affiliation"],
+                        "Senator's state": vote["State"],
+                    }
+                )
 
-        # A KÉT DATAFRAME EGYESÍTÉSE
-        merged_df = pd.merge(df_roll_call_votes, df_tally, on="Measure/Nominant Number", how="inner")
+        df_data = pd.DataFrame(flat_data)
 
         # CSV MENTÉSE
         current_folder = os.path.dirname(os.path.abspath(__file__))
-        merged_file_path = os.path.join(current_folder, "RollCallVotes.csv")
-        save_data_to_csv(merged_df, merged_file_path)
+        file_path = os.path.join(current_folder, "RollCallVotes.csv")
+        save_data_to_csv(df_data, file_path)
 
     finally:
         driver.quit()
+
 
 if __name__ == "__main__":
     main()
